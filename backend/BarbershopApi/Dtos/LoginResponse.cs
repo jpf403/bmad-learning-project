25565881e3cef376a0965aa6dae5d6c2632b5ee8
Jack <jack.formato@z-pax.com>
2026-09-09 14:08:56 -0400

    Missing file from previous commit

--- backend/BarbershopApi/Dtos/LoginResponse.cs
+++ backend/BarbershopApi/Dtos/LoginResponse.cs
@@ -2,4 +2,4 @@ using BarbershopApi.Entities;
 
 namespace BarbershopApi.Dtos;
 
-public record LoginResponse(string AccessToken, int Id, string Email, string FirstName, string LastName, Role Role);
+public record LoginResponse(string AccessToken, int Id, string Email, string FirstName, string LastName, Role Role, bool IsSsoLinked);
